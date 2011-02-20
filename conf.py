f61c9bfaa433c8e0e80bd74c3c4f01879da27323
shimizukawa <devnull@localhost>
2011-02-21 01:35:19 +0900

    fix: 2010 -> 2010-2011

--- conf.py
+++ conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Sphinx-Users.jp'
-copyright = u'2010, Sphinx-Users.jp'
+copyright = u'2010-2011, Sphinx-Users.jp'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
