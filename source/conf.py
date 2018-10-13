c4bc6a6820065e187dc5945c10abb8ba610ca2d1
usaturn <kane@berserga.com>
2018-10-13 18:16:53 +0900

    update copyright

--- source/conf.py
+++ source/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Sphinx-Users.jp'
-copyright = u'2010-2017, Sphinx-Users.jp'
+copyright = u'2010-2018, Sphinx-Users.jp'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
