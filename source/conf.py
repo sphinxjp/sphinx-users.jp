21c0087bbefee7a22fdd9f0ca77bbedb24d21b5d
usaturn <kane@berserga.com>
2020-01-18 22:30:49 +0900

    change directive

--- source/conf.py
+++ source/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Sphinx-Users.jp'
-copyright = u'2010-2019, Sphinx-Users.jp'
+copyright = u'2010-2020, Sphinx-Users.jp'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
