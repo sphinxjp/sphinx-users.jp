c30d6bcf6197eb961ea63c0c88e29a04d023f98b
usaturn <kane@berserga.com>
2020-02-23 13:19:02 +0900

    returnd description of extention

--- source/conf.py
+++ source/conf.py
@@ -22,10 +22,12 @@ sys.path.append(os.path.abspath('_ext'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo']
-extensions += ['ogtag']
-extensions += ['sphinxcontrib.gist']
-extensions += ['sphinxcontrib.trimblank']
+extensions = [
+    'sphinx.ext.todo',
+    'sphinxcontrib.trimblank',
+    'ogtag',
+    'sphinxcontrib.gist',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
